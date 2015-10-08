73c6a9eb527dee0356ae3ce2d819486c8114eeec
bmpasini <bmpasini@gmail.com>
2015-10-08 15:08:11 -0400

    improve code from lecture 4

--- src/week4/lectures/BST.java
+++ src/week4/lectures/BST.java
@@ -1,5 +1,6 @@
 package week4.lectures;
 
+import java.util.NoSuchElementException;
 import edu.princeton.cs.algs4.Queue;
 
 public class BST<Key extends Comparable<Key>, Value> {
@@ -15,53 +16,17 @@ public class BST<Key extends Comparable<Key>, Value> {
 		private Node left, right;
 		private int count;
 		
-		public Node (Key key, Value val) {
+		public Node (Key key, Value val, int count) {
 			this.key = key;
 			this.val = val;
+			this.count = count;
 		}
 		
 	}
 	
-	public void put(Key key, Value val) {
-		root = put(root, key, val);
-	}
-	
-	private Node put(Node x, Key key, Value val) {
-		if (x == null) return new Node(key, val);
-		int cmp = key.compareTo(x.key);
-		if      (cmp > 0) return put(x.left, key, val);
-		else if (cmp < 0) return put(x.right, key, val);
-		else                     x.val = val;
-		x.count = 1 + size(x.left) + size(x.right);
-		return x;
-	}
-	
-	public Value get(Key key) {
-		Node x = root;
-		while (x != null) {
-			int cmp = key.compareTo(x.key);
-			if      (cmp < 0) x = x.left;
-			else if (cmp > 0) x = x.right;
-			else              return x.val;
-		}
-		return null;
-	}
-	
-	public Key floor(Key key) {
-		Node x = floor(root, key);
-		if (x == null) return null;
-		return x.key;
-	}
-	
-	public Node floor(Node x, Key key) {
-		if (x == null) return null;
-		int cmp = key.compareTo(x.key);
-		if (cmp == 0) return x;
-		if (cmp < 0)  return floor(x.left, key);
-		Node t = floor(x.right, key);
-		if (t != null) return t;
-		else           return x;
-	}
+	public boolean isEmpty() {
+        return size() == 0;
+    }
 	
 	public int size() {
 		return size(root);
@@ -72,11 +37,159 @@ public class BST<Key extends Comparable<Key>, Value> {
 		return x.count;
 	}
 	
+	public boolean contains(Key key) {
+		return get(key) != null;
+	}
+	
+	public Value get(Key key) {
+		return get(root, key);
+	}
+	
+	private Value get(Node x, Key key) {
+		if (x == null) return null;
+		int cmp = key.compareTo(x.key);
+		if      (cmp < 0) return get(x.left, key);
+		else if (cmp > 0) return get(x.right, key);
+		else              return x.val;
+	}
+	
+	public void put(Key key, Value val) {
+		if (val == null) {
+			delete(key);
+			return;
+		}
+		root = put(root, key, val);
+	}
+	
+	private Node put(Node x, Key key, Value val) {
+		if (x == null) return new Node(key, val, 1);
+		int cmp = key.compareTo(x.key);
+		if      (cmp < 0) x.left  = put(x.left, key, val);
+		else if (cmp > 0) x.right = put(x.right, key, val);
+		else              x.val   = val;
+		x.count = 1 + size(x.left) + size(x.right);
+		return x;
+	}
+	
+	public void deleteMin() {
+		if (isEmpty()) throw new NoSuchElementException("Symbol table underflow");
+		root = deleteMin(root);
+	}
+	
+	private Node deleteMin(Node x) {
+		if (x.left == null) return x.right;
+		x.left = deleteMin(x.left);
+		x.count = 1 + size(x.left) + size(x.right);
+		return x;
+	}
+	
+	public void deleteMax() {
+		if (isEmpty()) throw new NoSuchElementException("Symbol table underflow");
+		root = deleteMax(root);
+	}
+	
+	private Node deleteMax(Node x) {
+		if (x.right == null) return x.left;
+		x.right = deleteMax(x.right);
+		x.count = 1 + size(x.left) + size(x.right);
+		return x;
+	}
+	
+	public void delete(Key key) {
+		root = delete(root, key);
+	}
+	
+	private Node delete(Node x, Key key) {
+		if (x == null) return null;
+		int cmp = key.compareTo(x.key);
+		if      (cmp < 0) x.left  = delete(x.left, key);
+		else if (cmp > 0) x.right = delete(x.right, key);
+		else {
+			if (x.left == null) return x.right;
+			if (x.right == null) return x.left;
+			// Hibbard deletion
+			Node t = x;
+			x = min(t.right);
+			x.right = deleteMin(t.right);
+			x.left = t.left;
+		}
+		x.count = 1 + size(x.left) + size(x.right);
+		return x;
+	}
+	
+	public Key min() {
+		if (isEmpty()) throw new NoSuchElementException("called min() with empty symbol table");
+		return min(root).key;
+	}
+	
+	private Node min(Node x) {
+		if (x.left == null) return x;
+		return min(x.left);
+	}
+	
+	public Key max() {
+		if (isEmpty()) throw new NoSuchElementException("called max() with empty symbol table");
+		return max(root).key;
+	}
+	
+	private Node max(Node x) {
+		if (x.right == null) return x;
+		return max(x.right);
+	}
+	
+	public Key floor(Key key) {
+		if (isEmpty()) throw new NoSuchElementException("called floor() with empty symbol table");
+		Node x = floor(root, key);
+		if (x == null) return null;
+		return x.key;
+	}
+	
+	private Node floor(Node x, Key key) {
+		if (x == null) return null;
+		int cmp = key.compareTo(x.key);
+		if (cmp == 0) return x;
+		if (cmp <  0) return floor(x.left, key);
+		Node t = floor(x.right, key);
+		if (t != null) return t;
+		else           return x;
+	}
+	
+	public Key ceiling(Key key) {
+		if (isEmpty()) throw new NoSuchElementException("called ceiling() with empty symbol table");
+		Node x = ceiling(root, key);
+		if (x == null) return null;
+		return x.key;
+	}
+	
+	private Node ceiling(Node x, Key key) {
+		if (x == null) return null;
+		int cmp = key.compareTo(x.key);
+		if (cmp == 0) return x;
+		if (cmp >  0) return floor(x.right, key);
+		Node t = floor(x.left, key);
+		if (t != null) return t;
+		else           return x;
+	}
+	
+	public Key select(int k) {
+		if (k < 0 || k >= size()) throw new IllegalArgumentException("Symbol table doesn't have such element");
+		Node x = select(root, k);
+		return x.key;
+	}
+	
+	private Node select(Node x, int k) {
+		if (x == null) return null;
+		int t = size(x.left);
+		if      (t < k) return select(x.left,  k);
+		else if (t > k) return select(x.right, k-t-1);
+		else            return x;
+	}
+	
 	public int rank(Key key) {
 		return rank(key, root);
 	}
 	
-	public int rank(Key key, Node x) {
+	private int rank(Key key, Node x) {
 		if (x == null) return 0;
 		int cmp = key.compareTo(x.key);
 		if      (cmp < 0) return rank(key, x.left);
@@ -97,56 +210,36 @@ public class BST<Key extends Comparable<Key>, Value> {
 		inorder(x.right, q);
 	}
 	
-	public void deleteMin() {
-		root = deleteMin(root);
-	}
+	public int height() {
+        return height(root);
+    }
 	
-	public Node deleteMin(Node x) {
-		if (x.left == null) return x.right;
-		x.left = deleteMin(x.left);
-		x.count = 1 + size(x.left) + size(x.right);
-		return x;
-	}
+    private int height(Node x) {
+        if (x == null) return -1;
+        return 1 + Math.max(height(x.left), height(x.right));
+    }
 	
-	public Node min(Node x) {
-		if (x.left == null) return x;
-		return min(x.left);
-	}
-	
-	public Node max(Node x) {
-		if (x.right == null) return x;
-		return max(x.right);
-	}
-	
-	public void delete(Key key) {
-		root = delete(key, root);
-	}
-	
-	public Node delete(Key key, Node x) {
-		if (x == null) return null;
-		int cmp = key.compareTo(x.key);
-		if      (cmp < 0) return delete(key, x.left);
-		else if (cmp > 0) return delete(key, x.right);
-		else {
-			if (x.left == null) return x.right;
-			if (x.right == null) return x.left;
-			// Hibbard deletion
-			Node t = x;
-			x = min(t.right);
-			x.right = deleteMin(t.right);
-			x.left = t.left;
-		}
-		x.count = 1 + size(x.left) + size(x.right);
-		return x;
-	}
+	public Iterable<Key> levelOrder() {
+        Queue<Key> keys = new Queue<Key>();
+        Queue<Node> q = new Queue<Node>();
+        q.enqueue(root);
+        while(!q.isEmpty()) {
+        	Node x = q.dequeue();
+        	if (x == null) continue;
+        	q.enqueue(x.left);
+        	q.enqueue(x.right);
+        	keys.enqueue(x.key);
+        }
+        return keys;
+    }
 
 	public static void main(String[] args) {
-		
-		edu.princeton.cs.algs4.BST<Integer,Integer> bst = new edu.princeton.cs.algs4.BST<>();
-//		BST<Integer,Integer> bst = new BST<>();
+		BST<Integer,Integer> bst = new BST<>();
 		Integer[] a = { 39, 40, 65, 51, 99, 38, 84, 75, 56, 55 };
 		for (int i = 0; i < a.length; i++) bst.put(a[i],i);
-		for (Integer key : bst.keys())
-			System.out.print(key + " ");
+		for (Integer k : bst.levelOrder())
+			System.out.print(k + " ");
+		System.out.println();
+//		System.out.print(key + "," + bst.get(key) + " ");
 	}
 }
